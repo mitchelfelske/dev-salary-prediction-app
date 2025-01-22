8f9c7a5ac884f169658c6eeffad2f7977ac91760
Mitchel Soni Felske <mitchel.felske@outlook.com>
2025-01-22 16:35:42 -0300

    Add prediction route with form data

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 # create an app instance
 app = Flask(__name__)
@@ -7,6 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = {
+        'country': request.form['country'],
+        'education': request.form['education'],
+        'devtype': request.form['devtype'],
+        'yearscode': request.form['yearscode']
+    }
+    return data
 
 # run the flask app
 if __name__ == "__main__":
