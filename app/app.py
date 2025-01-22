5ba8043c942ad0903d1525912b9f5a767f3f479f
Mitchel Soni Felske <mitchel.felske@outlook.com>
2025-01-22 17:05:51 -0300

    Add label encoding for form data

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, request
+import pickle
 
 # create an app instance
 app = Flask(__name__)
+
+# load the model & label encoding files
+model_file = pickle.load(open('../resources/model.pkl', 'rb'))
+
+country_mapping = model_file['country_mapping']
+education_mapping = model_file['education_mapping']
+devtype_mapping = model_file['devtype_mapping']
 
 # at the end point /, call method hello
 @app.route("/")
@@ -16,6 +24,12 @@ def predict():
         'devtype': request.form['devtype'],
         'yearscode': request.form['yearscode']
     }
+
+    # Label encoding - mapping categorical to numerical values
+    data['country'] = country_mapping[data['country']]
+    data['education'] = education_mapping[data['education']]
+    data['devtype'] = devtype_mapping[data['devtype']]
+    data['yearscode'] = float(data['yearscode'])
     return data
 
 # run the flask app
