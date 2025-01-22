fbacf4c8b6b5c8e88cb1818eacbbaeddf620f0d3
Mitchel Soni Felske <mitchel.felske@outlook.com>
2025-01-22 19:29:01 -0300

    Add feature scaling & model prediction

--- app/app.py
+++ app/app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 
 # load the model & label encoding files
 model_file = pickle.load(open('../resources/model.pkl', 'rb'))
-
+model = model_file['model']
 country_mapping = model_file['country_mapping']
 education_mapping = model_file['education_mapping']
 devtype_mapping = model_file['devtype_mapping']
+
+# load the scaler
+scaler = pickle.load(open('../resources/scaler.pkl', 'rb'))
 
 # at the end point /, call method hello
 @app.route("/")
@@ -30,7 +33,15 @@ def predict():
     data['education'] = education_mapping[data['education']]
     data['devtype'] = devtype_mapping[data['devtype']]
     data['yearscode'] = float(data['yearscode'])
-    return data
+
+    # Feature scaling
+    # The wrapped brackets are used to convert the data into a 2D array (lines, columns)
+    standardized_data = scaler.transform([list(data.values())])
+
+    # Model prediction
+    prediction = model.predict(standardized_data)
+
+    return "$ {} [valor anual]".format(round(prediction[0], 2))
 
 # run the flask app
 if __name__ == "__main__":
